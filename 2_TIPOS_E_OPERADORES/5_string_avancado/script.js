7aa846a7fef6dccc1d11db55f17af04636fce0a7
Pedro H. Pereira <phpereirasilva2@gmail.com>
2024-11-15 00:07:32 -0300

    "types and operators" section completed

---
+++ 2_TIPOS_E_OPERADORES/5_string_avancado/script.js
@@ -0,0 +1,5 @@
+console.log("Primeira linha \nSegunda linha");
+console.log("O meu nome é 'Pedro'");
+console.log('O nome dela é "Roberta"');
+console.log(`A multiplicação de 5 por 3 é ${5 * 3}`);
+console.log("O " + " meu " + " nome " + " é " + " Pedro.");
---
+++ 2_TIPOS_E_OPERADORES/9_boolean_op_ternario/script.js
@@ -0,0 +1,3 @@
+console.log(5 > 2 ? 'É sim' : 'É não');
+console.log(false ? 5 : 4);
+console.log('Pedro' == 'Pedro' ? 'Olá, Pedro!' : 'Não é o Pedro!');
