bbd9a8226b4d02795b5c7a98da05bbd1514c7e26
Pedro H. Pereira <phpereirasilva2@gmail.com>
2024-11-15 00:09:01 -0300

    "exercises of types and operators" section completed

---
+++ 3_EXERCICIOS_TIPOS_E_OPERADORES/1_exercicio_01/script.js
@@ -0,0 +1,3 @@
+console.log("String com aspas duplas");
+console.log('String com aspas comum');
+console.log(`String com template literals`);
---
+++ 3_EXERCICIOS_TIPOS_E_OPERADORES/4_exercicio_04/script.js
@@ -0,0 +1,3 @@
+console.log(3 > 2 && 3 <= 4);
+console.log(2 < 2 || 2 == 2);
+console.log(false != !true);
