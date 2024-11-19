0204fb9679f47c7eb4df6fe81896661cad903151
Pedro H. Pereira <phpereirasilva2@gmail.com>
2024-11-19 20:42:07 -0300

    "debugs and bugs" section completed

---
+++ 12_DEBUGS_E_ERROS/5_exception/script.js
@@ -0,0 +1,12 @@
+function saudacao(nome) {
+    if (typeof nome != 'string') {
+        throw new Error('O parâmetro nome precisa ser string');
+    } else {
+        console.log(`Olá ${nome}.`)
+    }
+}
+
+saudacao('Pedro');
+saudacao(5);
+
+console.log('teste')
